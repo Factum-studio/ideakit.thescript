1a46c656f64ae6dccfc1da4c99f9b869a26a5ec9
Кузнецов Данила Алексеевич <50800526+DoctorDanila@users.noreply.github.com>
2026-08-09 08:26:54 +0500

    fix: uncle stan
    - Дядя стэн развыёбывался и мы угомонили буйного (понизили уровень строгости)

---
+++ phpstan-bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+try {
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+    $dotenv->load();
+} catch (Dotenv\Exception\InvalidPathException $e) {
+    throw $e;
+}
+
+defined('YII_DEBUG') or define('YII_DEBUG', $_ENV['APP_DEBUG'] ?? true);
+defined('YII_ENV') or define('YII_ENV', $_ENV['APP_ENV'] ?? 'dev');
