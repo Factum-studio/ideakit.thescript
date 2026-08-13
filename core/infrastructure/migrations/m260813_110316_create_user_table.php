7007a57469d346c8ae6e74d2842f9ce03ecc29c0
Кузнецов Данила Алексеевич <50800526+DoctorDanila@users.noreply.github.com>
2026-08-14 04:11:51 +0500

    ADD BASE USER MIGRATIONS  (#17)
    * style: rm old gitkeepers
    
    * feat: create user migrations #6
    
    * feat: update migration config #6
    
    * feat: update uncle stan
    
    Дорабатываем конфигурацию дяди стэна, чтобы он не волновался, если не нашёл ошибок из списка игнорирования
    
    * feat: run linter #6
    
    * feat: update migrations #6
    
    Меняем тип ключей на UUIDv7
    
    * docs: update tech spec mvp #6
    
    - Обновлена erd диаграмма
    - Обновлена тех. спецификация mvp проекта
    
    * fix: connection review comments #6

---
+++ core/infrastructure/migrations/m260813_110316_create_user_table.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace core\infrastructure\migrations;
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%user}}`.
+ */
+class m260813_110316_create_user_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%user}}', [
+            'id'            => $this->string(36)->notNull(),
+            'surname'       => $this->string(100)->notNull(),
+            'name'          => $this->string(100)->notNull(),
+            'patronymic'    => $this->string(100)->null(),
+            'email'         => $this->string(255)->null()->unique(),
+            'phone'         => $this->string(20)->null()->unique(),
+            'role'          => $this->string(50)->notNull()->defaultValue('user'),
+            'post'          => $this->string(100)->null(),
+            'status'        => $this->smallInteger()->notNull()->defaultValue(1)->comment('1 – active, 0 – inactive'),
+            'auth_key'      => $this->string(32)->notNull(),
+            'created_at'    => $this->timestamp()->notNull()->defaultExpression('CURRENT_TIMESTAMP'),
+            'updated_at'    => $this->timestamp()->notNull()->defaultExpression('CURRENT_TIMESTAMP'),
+            'last_login_at' => $this->timestamp()->null(),
+        ]);
+
+        $this->addPrimaryKey('pk-user', '{{%user}}', 'id');
+
+        // Меняем тип на uuid (PostgreSQL)
+        if ($this->getDb()->getDriverName() === 'pgsql') {
+            $this->execute('ALTER TABLE {{%user}} ALTER COLUMN id TYPE uuid USING id::uuid');
+        }
+
+        // Индексы
+        $this->createIndex('idx-user-email', '{{%user}}', 'email');
+        $this->createIndex('idx-user-phone', '{{%user}}', 'phone');
+        $this->createIndex('idx-user-status', '{{%user}}', 'status');
+        $this->createIndex('idx-user-auth_key', '{{%user}}', 'auth_key');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropIndex("idx-user-auth_key", '{{%user}}');
+        $this->dropIndex("idx-user-status", '{{%user}}');
+        $this->dropIndex("idx-user-phone", '{{%user}}');
+        $this->dropIndex("idx-user-email", '{{%user}}');
+        $this->dropTable('{{%user}}');
+    }
+}
