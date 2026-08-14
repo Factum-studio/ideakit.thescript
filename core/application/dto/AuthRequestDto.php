d9eb1879d7ea6d4a7746f6a4350ab54ad83e4258
Кузнецов Данила Алексеевич <50800526+DoctorDanila@users.noreply.github.com>
2026-08-15 00:55:16 +0500

    feat: add auth related dtos #9
    - Auth Request and Responce dto's

---
+++ core/application/dto/AuthRequestDto.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace core\application\dto;
+
+class AuthRequestDto
+{
+    public function __construct(
+        public string $provider,
+        public string $providerClientId,
+        public array $userData = [] // surname, name, patronymic, email, phone, etc.
+    ) {
+    }
+}
---
+++ core/application/dto/AuthResponseDto.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace core\application\dto;
+
+use JsonSerializable;
+
+class AuthResponseDto implements JsonSerializable
+{
+    public function __construct(
+        public string $accessToken,
+        public array $user
+    ) {
+    }
+
+    public function jsonSerialize(): array
+    {
+        return [
+            'access_token'  => $this->accessToken,
+            'user'          => $this->user,
+        ];
+    }
+}
