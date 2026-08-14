bd783f45d8d9552eeba06c1cd4bb49f4ef6834fa
Кузнецов Данила Алексеевич <50800526+DoctorDanila@users.noreply.github.com>
2026-08-15 01:05:49 +0500

    feat: prepare user identity command #9
    Добавление Identity для пользователя

---
+++ core/application/command/AddUserIdentityCommand.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace core\application\command;
+
+class AddUserIdentityCommand
+{
+    public function __construct(
+        public string $userId,
+        public string $provider,
+        public string $providerClientId
+    ) {
+    }
+}
