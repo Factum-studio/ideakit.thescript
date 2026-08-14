192ffd9ad1634070007b53c87e078bf847f178e8
Кузнецов Данила Алексеевич <50800526+DoctorDanila@users.noreply.github.com>
2026-08-15 01:06:59 +0500

    feat: prepare user queries #9
    - Получение пользователя
    - Поиск пользователя

---
+++ core/application/query/FindUserQuery.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace core\application\query;
+
+class FindUserQuery
+{
+    public function __construct(
+        public ?string $email       = null,
+        public ?string $phone       = null,
+        public ?string $role        = null,
+        public ?string $post        = null,
+        public ?int $status         = null,
+        public ?string $createdFrom = null,
+        public ?string $createdTo   = null,
+        public ?string $updatedFrom = null,
+        public ?string $updatedTo   = null,
+        public ?string $lastLoginFrom = null,
+        public ?string $lastLoginTo = null,
+        public int $limit           = 20,
+        public int $page            = 1,
+        public ?array $orderBy      = null
+    ) {
+    }
+}
