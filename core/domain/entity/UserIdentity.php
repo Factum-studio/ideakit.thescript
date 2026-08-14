b20edfb0a6cb4c26d723e13795b5a17bc0745373
Кузнецов Данила Алексеевич <50800526+DoctorDanila@users.noreply.github.com>
2026-08-15 00:50:47 +0500

    feat: add user identity entity #9

---
+++ core/domain/entity/UserIdentity.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace core\domain\entity;
+
+use core\domain\valueObject\UserId;
+use core\domain\valueObject\UserIdentityId;
+use DateTimeImmutable;
+
+class UserIdentity
+{
+    private UserIdentityId $id;
+    private UserId $userId;
+    private string $provider;
+    private string $providerClientId;
+    private DateTimeImmutable $createdAt;
+
+    public function __construct(
+        UserIdentityId $id,
+        UserId $userId,
+        string $provider,
+        string $providerClientId,
+        DateTimeImmutable $createdAt
+    ) {
+        $this->id               = $id;
+        $this->userId           = $userId;
+        $this->provider         = $provider;
+        $this->providerClientId = $providerClientId;
+        $this->createdAt        = $createdAt;
+    }
+
+    public function getId(): UserIdentityId { return $this->id; }
+    public function getUserId(): UserId { return $this->userId; }
+    public function getProvider(): string { return $this->provider; }
+    public function getProviderClientId(): string { return $this->providerClientId; }
+    public function getCreatedAt(): DateTimeImmutable { return $this->createdAt; }
+}
