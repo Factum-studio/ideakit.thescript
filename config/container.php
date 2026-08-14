835970f50e5ef56c6e890f604ea9565a06ca9d01
Кузнецов Данила Алексеевич <50800526+DoctorDanila@users.noreply.github.com>
2026-08-15 01:32:39 +0500

    feat: register user repositories in di #9

--- config/container.php
+++ config/container.php
@@ -2,5 +2,18 @@
 
 declare(strict_types=1);
 
+use core\application\port\IUserIdentityRepository;
+use core\application\port\IUserRepository;
+use core\infrastructure\repository\DbUserIdentityRepository;
+use core\infrastructure\repository\DbUserRepository;
 $container = Yii::$container;
+
+// ---------- Репозитории ----------
+$container->setSingleton(IUserRepository::class, function () {
+    return new DbUserRepository();
+});
+
+$container->setSingleton(IUserIdentityRepository::class, function () {
+    return new DbUserIdentityRepository();
+});
 
