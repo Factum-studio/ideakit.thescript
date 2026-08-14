9ce16eb06e7e012c9b0852a180fe919171439f28
Кузнецов Данила Алексеевич <50800526+DoctorDanila@users.noreply.github.com>
2026-08-15 01:52:48 +0500

    feat: register heandlers in di #9

--- config/container.php
+++ config/container.php
@@ -2,6 +2,14 @@
 
 declare(strict_types=1);
 
+use core\application\handler\AddUserIdentityHandler;
+use core\application\handler\CreateUserHandler;
+use core\application\handler\DeleteUserHandler;
+use core\application\handler\FindUserHandler;
+use core\application\handler\GetUserByIdentityHandler;
+use core\application\handler\GetUserHandler;
+use core\application\handler\RegenerateAuthKeyHandler;
+use core\application\handler\UpdateUserHandler;
 use core\application\port\ISecurityService;
 use core\application\port\IUserIdentityRepository;
 use core\application\port\IUserRepository;
@@ -22,5 +30,60 @@ $container->setSingleton(IUserIdentityRepository::class, function () {
 // ---------- Security ----------
 $container->setSingleton(ISecurityService::class, function () {
     return new YiiSecurityService();
+});
+
+// ---------- Хендлеры (команды и запросы) ----------
+$container->set(CreateUserHandler::class, function () use ($container) {
+    return new CreateUserHandler(
+        $container->get(IUserRepository::class),
+        $container->get(ISecurityService::class),
+    );
+});
+
+$container->set(UpdateUserHandler::class, function () use ($container) {
+    return new UpdateUserHandler(
+        $container->get(IUserRepository::class),
+    );
+});
+
+$container->set(DeleteUserHandler::class, function () use ($container) {
+    return new DeleteUserHandler(
+        $container->get(IUserRepository::class),
+        $container->get(IUserIdentityRepository::class),
+    );
+});
+
+$container->set(AddUserIdentityHandler::class, function () use ($container) {
+    return new AddUserIdentityHandler(
+        $container->get(IUserIdentityRepository::class),
+        $container->get(IUserRepository::class),
+    );
+});
+
+$container->set(GetUserHandler::class, function () use ($container) {
+    return new GetUserHandler(
+        $container->get(IUserRepository::class),
+        $container->get(IUserIdentityRepository::class),
+    );
+});
+
+$container->set(FindUserHandler::class, function () use ($container) {
+    return new FindUserHandler(
+        $container->get(IUserRepository::class),
+    );
+});
+
+$container->set(GetUserByIdentityHandler::class, function () use ($container) {
+    return new GetUserByIdentityHandler(
+        $container->get(IUserIdentityRepository::class),
+        $container->get(IUserRepository::class),
+    );
+});
+
+$container->set(RegenerateAuthKeyHandler::class, function () use ($container) {
+    return new RegenerateAuthKeyHandler(
+        $container->get(IUserRepository::class),
+        $container->get(ISecurityService::class),
+    );
 });
 
