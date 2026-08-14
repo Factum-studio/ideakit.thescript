e6c48e2a33c622fa49bcbb91babf6b200c59073e
Кузнецов Данила Алексеевич <50800526+DoctorDanila@users.noreply.github.com>
2026-08-15 00:41:46 +0500

    feat: add more basic exceptions #9
    - Добавлено исключение валидации
    - Добавлено исключение пользователь не найден
    - Добавлено исключение не найдена identity
    - Добавлено исключение пользователь уже существует
    - Добавлено исключение неверные аутентификационные данные 401 код
    - Добавлено исключение Identity уже существует

---
+++ core/domain/exception/UserNotFoundException.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace core\domain\exception;
+
+use Throwable;
+
+class UserNotFoundException extends DomainException
+{
+    public function __construct(
+        string     $message     = 'User not found',
+        int        $code        = 0,
+        ?Throwable $previous    = null)
+    {
+        parent::__construct($message, 404, $code, $previous);
+    }
+}
---
+++ core/domain/exception/ValidationException.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace core\domain\exception;
+
+use Throwable;
+
+class ValidationException extends DomainException
+{
+    private array $errors;
+
+    public function __construct(
+        string     $message     = 'Validation failed',
+        array      $errors      = [],
+        int        $code        = 0,
+        ?Throwable $previous    = null)
+    {
+        $this->errors = $errors;
+        parent::__construct($message, 400, $code, $previous);
+    }
+
+    public function getErrors(): array
+    {
+        return $this->errors;
+    }
+}
