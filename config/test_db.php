212a079a91c9605465a21a5908791a8f19116c60
Кузнецов Данила Алексеевич <50800526+DoctorDanila@users.noreply.github.com>
2026-08-15 05:47:04 +0500

    feat: configure test circuit #10
    - Правим конфиг использования тестовой бд
    - Удаляем не нужные тесты базового шаблона

--- config/test_db.php
+++ config/test_db.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 $db = require __DIR__ . '/db.php';
 // test database! Important not to run tests on production or development databases
-$db['dsn'] = 'mysql:host=localhost;dbname=yii2basic_test';
+$db['dsn']      = 'pgsql:host=localhost;dbname=ideakit_test';
+$db['username'] = $_ENV['DB_USERNAME'] ?? 'ideakit';
+$db['password'] = $_ENV['DB_PASSWORD'] ?? 'local-development-only';
 
 return $db;
