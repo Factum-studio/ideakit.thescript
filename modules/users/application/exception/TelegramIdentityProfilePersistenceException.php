08d45053e38b4ab7dd1ec31507c1eec6b7808628
SteveeMoore <stevenmoore@ya.ru>
2026-09-04 10:00:28 +0500

    feat: define Telegram profile persistence contract

---
+++ modules/users/application/exception/TelegramIdentityProfilePersistenceException.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace modules\users\application\exception;
+
+use RuntimeException;
+
+final class TelegramIdentityProfilePersistenceException extends RuntimeException
+{
+}
